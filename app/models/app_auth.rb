c903427f7683cb2556b1012f8b4bac6fded1d400
Henrique Santana <hsatana.sousass@gmail.com>
2024-01-21 23:37:56 -0300

    feat: model para autenticação do app

---
+++ app/models/app_auth.rb
@@ -0,0 +1,26 @@
+class AppAuth < ApplicationRecord
+  has_secure_password :key, validations: false
+
+  before_create :code_generate
+
+  PRAZO_KEY = 10.minutes
+
+  def prazo_valido?
+    (Time.new - last_created_key_at) <= PRAZO_KEY unless last_created_key_at.nil?
+  end
+
+  def ativar!
+    self.ativo = true
+    self.activation_at = Time.new
+
+    self.save
+  end
+
+  private
+
+  def code_generate
+    self.code = rand(100000..999999).to_s
+
+    code_generate if AppAuth.exists?(code: code)
+  end
+end
