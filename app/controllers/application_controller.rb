253a1cacf1761da8978d0fa900a32f2ef0360b8e
Henrique Santana <hsatana.sousass@gmail.com>
2024-01-21 23:23:13 -0300

    feat: render erro chave expirada

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+  def erro_key_expirada
+    render(json: { message: 'Chave expirou! Solicite ao administrador que gere outra chave.' }, status: :bad_request)
+  end
 end
