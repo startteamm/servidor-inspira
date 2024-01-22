7b18c3c30a2e034f27f3951f5ad54d7ffa445b40
riquedss <94206402+riquedss@users.noreply.github.com>
2024-01-21 23:37:56 -0300

    feat: model para autenticação do app

---
+++ db/migrate/20240120040749_create_app_auths.rb
@@ -0,0 +1,14 @@
+class CreateAppAuths < ActiveRecord::Migration[7.1]
+  def change
+    create_table :app_auths, id: false do |t|
+      t.string :id, null: false, primary_key: true, default: -> { "UUID()" }
+      t.string :code, null: false
+      t.string :key_digest
+      t.timestamp :last_created_key_at
+      t.boolean :ativo, default: false
+      t.timestamp :activation_at
+
+      t.timestamps
+    end
+  end
+end
