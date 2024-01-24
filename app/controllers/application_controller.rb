42d27dc8ee735fa5eed260ba668d8cc209205860
Rodrigo Barroso <digorbarroso@gmail.com>
2024-01-24 02:07:24 -0300

    login wip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    skip_before_action :verify_authenticity_token
+
+    protected
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name, :phone, :university, :provider, :uid])
+    end
 end
+  
