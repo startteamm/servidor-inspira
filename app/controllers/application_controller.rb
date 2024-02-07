7a80dd2c91ef6ba9274c8a69a85c3262d6d7c898
Henrique Santana <hsatana.sousass@gmail.com>
2024-02-06 23:01:09 -0300

    feat: autentica app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::API
   def erro_key_expirada
     render(json: { message: 'Chave expirou! Solicite ao administrador que gere outra chave.' }, status: :bad_request)
   end
+
+  def authenticate_app!
+    app = Jwt::Base.decode(request.headers[:Authorization])
+
+    return head(:unauthorized) unless !app.nil? && @app = AppAuth.find(app[0]["id"])
+  end
 end
