58274f804a2584e9747fbb3657595369ba3f799c
riquedss <94206402+riquedss@users.noreply.github.com>
2024-02-07 00:51:27 -0300

    feat: adiciona campos para as credeciais do organizador

--- db/migrate/20240120040749_create_app_auths.rb
+++ db/migrate/20240120040749_create_app_auths.rb
@@ -2,6 +2,8 @@ class CreateAppAuths < ActiveRecord::Migration[7.1]
   def change
     create_table :app_auths, id: false do |t|
       t.string :id, null: false, primary_key: true, default: -> { "UUID()" }
+      t.string :employee_name
+      t.integer :celula
       t.string :code, null: false
       t.string :key_digest
       t.timestamp :last_created_key_at
