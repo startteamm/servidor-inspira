c7643da67dc465e9c4a57a70ee8fbc71b1a66c2b
Rodrigo Barroso <digorbarroso@gmail.com>
2024-03-02 17:28:16 -0300

    add home links

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,13 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+
+  get '/lojinha', to: 'shop#index'
+  get '/ingressos', to: 'ticket#index'
+  get '/programacao', to: 'schedule#index'
+  get '/quem-somos', to: 'about#index'
+  get '/ultimas-edicoes', to: 'about#past_events'
+  get '/equipes', to: 'about#teams'
+  get '/faq', to: 'about#faq'
+  get '/ajuda', to: 'about#help'
 end
