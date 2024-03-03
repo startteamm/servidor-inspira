ac1b98b7b609455f959553beb888ea7a6cfc27be
Henrique Santana <hsatana.sousass@gmail.com>
2024-03-03 04:26:33 -0300

    feat: routes de ingresso e tipo de ingresso

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,20 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  get 'app_auth/codigo' => 'app_auth#codigo'
-  post 'app_auth/validar_chave' => 'app_auth#validar_chave'
-  get 'app_auth/credenciais_organizador' => 'app_auth#credenciais_organizador'
+  get 'app_auth/codigo', to: 'app_auth#codigo'
+  post 'app_auth/validar_chave', to: 'app_auth#validar_chave'
+  get 'app_auth/credenciais_organizador', to: 'app_auth#credenciais_organizador'
 
   namespace :admin do
-    post 'ativacao_app' => 'app_auth#ativacao_app'
+    post 'ativacao_app', to: 'app_auth#ativacao_app'
+  end
+
+  namespace :api do
+    namespace :v1 do
+      resources :type_ticket
+      resources :ticket
+      get 'ticket/:id/qrcode', to: 'ticket#qrcode'
+      put 'ticket/:code/validar_ingresso', to: 'ticket#validar_ingresso'
+    end
   end
 end
