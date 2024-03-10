1c5787f50939014695a9a149f6e8245298189ca8
riquedss <94206402+riquedss@users.noreply.github.com>
2024-03-10 16:35:24 -0300

    chore: configurado letter_opener temporariamente para produção

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,16 +6,16 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
-    domain: 'inspiradesignuff.com',
-    user_name: Rails.application.credentials.dig(:google, :mailer_email),
-    password: Rails.application.credentials.dig(:google, :mailer_email_password),
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   address: 'smtp.gmail.com',
+  #   port: 587,
+  #   domain: 'inspiradesignuff.com',
+  #   user_name: Rails.application.credentials.dig(:google, :mailer_email),
+  #   password: Rails.application.credentials.dig(:google, :mailer_email_password),
+  #   authentication: :plain,
+  #   enable_starttls_auto: true
+  # }
 
   config.action_mailer.default_url_options = { host: "https://www.inspiradesignuff.com" }
 
@@ -101,4 +101,7 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
 end
