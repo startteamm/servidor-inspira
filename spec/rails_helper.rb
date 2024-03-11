3232ce62c93947caa09472addfdebebc79dd3143
riquedss <94206402+riquedss@users.noreply.github.com>
2024-03-11 01:09:58 -0300

    test (swagger): atualizadq url padrão

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,7 +31,7 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_paths = [
+  config.fixture_path = [
     Rails.root.join('spec/fixtures')
   ]
 
