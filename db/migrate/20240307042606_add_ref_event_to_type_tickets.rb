81270d24b28389281de2d817fde5fa45019053cd
riquedss <hsantana.sousass@gmail.com>
2024-03-23 18:31:22 -0300

    fix: correção nas mingrações da versão do rails

--- db/migrate/20240307042606_add_ref_event_to_type_tickets.rb
+++ db/migrate/20240307042606_add_ref_event_to_type_tickets.rb
@@ -1,4 +1,4 @@
-class AddRefEventToTypeTickets < ActiveRecord::Migration[7.1]
+class AddRefEventToTypeTickets < ActiveRecord::Migration[7.0]
   def change
     add_reference :type_tickets, :event, null: false, foreign_key: true
   end
