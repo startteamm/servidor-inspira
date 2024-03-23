4296e0835d95dd7d3f304b7e74c15389dd973c82
riquedss <hsantana.sousass@gmail.com>
2024-03-23 18:31:22 -0300

    fix: correção nas mingrações da versão do rails

--- db/migrate/20240307035233_create_events.rb
+++ db/migrate/20240307035233_create_events.rb
@@ -1,4 +1,4 @@
-class CreateEvents < ActiveRecord::Migration[7.1]
+class CreateEvents < ActiveRecord::Migration[7.0]
   def change
     create_table :events do |t|
       t.string :name, null: false
