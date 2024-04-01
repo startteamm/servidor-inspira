cf4f7b92a467cac308934689f29c2b824c70f77d
Rodrigo Barroso <digorbarroso@gmail.com>
2024-04-01 20:08:05 -0300

    adds end_time

--- db/migrate/20240307011640_create_activities.rb
+++ db/migrate/20240307011640_create_activities.rb
@@ -3,11 +3,11 @@ class CreateActivities < ActiveRecord::Migration[7.0]
     create_table :activities do |t|
       t.string :title, null: false
       t.text :description, null: false
-      t.string :duration, null: false
       t.string :image
       t.date :date, null: false
       t.string :workload, null: false
       t.time :start_time, null: false
+      t.time :end_time, null: false
       t.string :location, null: false
       t.integer :capacity, null: false
 
