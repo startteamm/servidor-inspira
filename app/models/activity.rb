604f7d94e6d1259282a3bffba9ed1968c13b7e10
riquedss <94206402+riquedss@users.noreply.github.com>
2024-04-14 03:39:03 -0300

    fix: validação em atributo não existente

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,6 +2,6 @@ class Activity < ApplicationRecord
     has_one :activity_type
     has_and_belongs_to_many :users
     has_and_belongs_to_many :guests
-    validates :title, :description, :duration, :date, :start_time, 
+    validates :title, :description, :date, :start_time, 
               :workload, :location, :capacity, :activity_type_id, presence: true
 end
