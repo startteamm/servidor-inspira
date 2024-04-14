9641eb9f658288a4ca8e957eb0becc70df060d24
Rodrigo Barroso <digorbarroso@gmail.com>
2024-04-14 16:43:48 -0300

    fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -14,11 +14,11 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_14_051614) do
   create_table "activities", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title", null: false
     t.text "description", null: false
-    t.string "duration", null: false
     t.string "image"
     t.date "date", null: false
     t.string "workload", null: false
     t.time "start_time", null: false
+    t.time "end_time", null: false
     t.string "location", null: false
     t.integer "capacity", null: false
     t.datetime "created_at", null: false
