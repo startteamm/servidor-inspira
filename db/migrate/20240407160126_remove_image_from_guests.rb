5918e188f53290e1f9878e02f517fc68967cdfc9
Rodrigo Barroso <digorbarroso@gmail.com>
2024-04-14 02:18:16 -0300

    migration fixes

--- db/migrate/20240407160126_remove_image_from_guests.rb
+++
@@ -1,5 +0,0 @@
-class RemoveImageFromGuests < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :guests, :image, :string
-  end
-end
--- db/migrate/20240409024800_change_image_to_binary.rb
+++
@@ -1,5 +0,0 @@
-class ChangeImageToBinary < ActiveRecord::Migration[7.0]
-  def change
-    change_column :activities, :image, :binary, limit: 20.megabytes
-  end
-end
