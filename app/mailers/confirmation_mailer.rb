0e0ce2ffc370407e1eccee9f9f387a7fc649d19c
Rodrigo Barroso <digorbarroso@gmail.com>
2024-04-14 02:07:23 -0300

    Changes email confirmation subject

--- app/mailers/confirmation_mailer.rb
+++ app/mailers/confirmation_mailer.rb
@@ -1,6 +1,6 @@
 class ConfirmationMailer < Devise::Mailer
     def confirmation_instructions(record, token, opts={})
-        opts[:subject] = "Confirmação de conta"
+        opts[:subject] = "Ative sua conta"
         opts[:from] = 'Inspira Design <naoresponda.inspiradesign@gmail.com>'
         super
     end
